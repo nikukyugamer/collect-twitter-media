d93df158c631bdb8b0a4c909a3cd99843a0eb446
Osamu Takiya <takiya@toran.sakura.ne.jp>
2018-04-09 16:44:37 +0900

    [update] refactor test code
    refactored test code

--- spec/collect_twitter_media_spec.rb
+++ spec/collect_twitter_media_spec.rb
@@ -1,5 +1,6 @@
 RSpec.describe CollectTwitterMedia do
-  it "has a version number" do
+  describe 'gem'
+  it "バージョン番号が設定されている" do
     expect(CollectTwitterMedia::VERSION).not_to be nil
   end
 
@@ -22,24 +23,26 @@ RSpec.describe CollectTwitterMedia do
     ]
   end
 
-  describe 'Define Twitter API client' do
-    it 'consumer_key is defined correctly' do
-      expect(CollectTwitterMedia.consumer_key('ABCDEFGHIJKLMN12345678')).to eq('ABCDEFGHIJKLMN12345678')
+  describe 'Twitter API クライアント' do
+    context 'Key や Token' do
+      it 'consumer_key が正しく設定されている' do
+        expect(CollectTwitterMedia.consumer_key('ABCDEFGHIJKLMN12345678')).to eq('ABCDEFGHIJKLMN12345678')
+      end
+
+      it 'consumer_secret が正しく設定されている' do
+        expect(CollectTwitterMedia.consumer_secret('ABCDEFGHIJKLMNOPQRSTUVWXYZ12345678901234')).to eq('ABCDEFGHIJKLMNOPQRSTUVWXYZ12345678901234')
+      end
+
+      it 'access_token が正しく設定されている' do
+        expect(CollectTwitterMedia.access_token('1234567-ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890123456')).to eq('1234567-ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890123456')
+      end
+
+      it 'access_token_secret が正しく設定されている' do
+        expect(CollectTwitterMedia.access_token_secret('ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890123456789')).to eq('ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890123456789')
+      end
     end
 
-    it 'consumer_secret is defined correctly' do
-      expect(CollectTwitterMedia.consumer_secret('ABCDEFGHIJKLMNOPQRSTUVWXYZ12345678901234')).to eq('ABCDEFGHIJKLMNOPQRSTUVWXYZ12345678901234')
-    end
-
-    it 'access_token is defined correctly' do
-      expect(CollectTwitterMedia.access_token('1234567-ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890123456')).to eq('1234567-ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890123456')
-    end
-
-    it 'access_token_secret is defined correctly' do
-      expect(CollectTwitterMedia.access_token_secret('ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890123456789')).to eq('ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890123456789')
-    end
-
-    it 'Twitter API client is defined correctly' do
+    it '正しく定義されている' do
       CollectTwitterMedia.consumer_key('ABCDEFGHIJKLMN12345678')
       CollectTwitterMedia.consumer_secret('ABCDEFGHIJKLMNOPQRSTUVWXYZ12345678901234')
       CollectTwitterMedia.access_token('1234567-ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890123456')
@@ -48,142 +51,124 @@ RSpec.describe CollectTwitterMedia do
     end
   end
 
-  describe 'Access to Twitter API' do
-    # under writing...
-  end
+  describe 'CollectTweetMedia のクラスメソッド' do
+    describe 'tweetsオブジェクト' do
+      context '#tweet_id_collection' do
+        it 'tweet_id_collectionメソッドは、tweetsオブジェクトに含まれているツイートIDを配列で返す' do
+          allow(@tweet_mock).to receive(:id)
+          expect(CollectTwitterMedia.tweet_id_collection(@tweets_mock).class).to eq(Array)
+          expect(CollectTwitterMedia.tweet_id_collection(@tweets_mock).count).to eq(@tweets_mock.count)
+        end
 
-  describe 'Operation of tweets object' do
-    it 'the structure of Tweet_id_collection is correct' do
-      allow(@tweet_mock).to receive(:id).and_return('661902288961871873')
-      expect(CollectTwitterMedia.tweet_id_collection(@tweets_mock).class).to eq(Array)
-      expect(CollectTwitterMedia.tweet_id_collection(@tweets_mock).count).to eq(@tweets_mock.count)
-    end
+        context '#(min|max)_tweet_id' do
+          it 'min_tweet_idメソッドは、ツイートIDを要素とする配列から、最小のツイートIDの値を抽出する' do
+            expect(CollectTwitterMedia.min_tweet_id(@tweet_id_collection)).to eq('661902288961871871')
+          end
 
-    it 'Min_tweet_id is picked up' do
-      expect(CollectTwitterMedia.min_tweet_id(@tweet_id_collection)).to eq('661902288961871871')
-    end
-
-    it 'Max_tweet_id is picked up' do
-      expect(CollectTwitterMedia.max_tweet_id(@tweet_id_collection)).to eq('661902288961871875')
-    end
-
-    context 'when retweet is replaced to original tweet' do
-      it 'this tweet is original tweet' do
-        allow(@tweet_mock).to receive(:retweet?).and_return(false)
-
-        twitter_client_mock = double('Twitter API Client Mock')
-        allow(CollectTwitterMedia).to receive(:twitter_client).and_return(twitter_client_mock)
-
-        @client = CollectTwitterMedia.twitter_client
-        expect(CollectTwitterMedia.original_tweet(@tweet_mock)).to eq(@tweet_mock)
-      end
-
-      # TODO: write
-      # it 'original tweet is retweet' do
-      #   allow(@tweet_mock).to receive(:retweet?).and_return(true)
-      # end
-    end
-
-    context 'when uris are retrieved' do
-      it 'the media uris are picked up if they exist' do
-        # dirty... should use 'Factory Bot'...
-        media = [
-          @tweet_media_photo_mock,
-          @tweet_media_photo_mock,
-          @tweet_media_photo_mock,
-          @tweet_media_photo_mock,
-        ]
-        return_uri = 'https://pbs.twimg.com/media/CS-Mn7iW4AA3Tak.png'
-        expected_return = [
-          return_uri,
-          return_uri,
-          return_uri,
-          return_uri,
-        ]
-
-        allow(@tweet_mock).to receive('media?').and_return(true)
-        allow(@tweet_mock).to receive(:media).and_return(media)
-        allow(@tweet_media_photo_mock).to receive('instance_of?').with(Twitter::Media::Photo).and_return(true)
-        allow(@tweet_media_photo_mock).to receive(:media_url_https).and_return(return_uri)
-
-        expect((CollectTwitterMedia.media_uris(@tweet_mock))).to eq(expected_return)
-      end
-
-      # it 'the media uri is nothing if it doesn't exist' do
-        # TODO: write
-      # end
-    end
-  end
-
-  describe 'Exchange text by other format' do
-    context "Exchange 'raw' media uri" do
-      it "to 'original' media uri" do
-        raw_media_uri = 'https://pbs.twimg.com/media/CS-Mn7iW4AA3Tak.png'
-        expect(CollectTwitterMedia.media_original_uri(raw_media_uri)).to eq('https://pbs.twimg.com/media/CS-Mn7iW4AA3Tak.png:orig')
-      end
-
-      it "to media filename with extension" do
-        raw_media_uri = 'https://pbs.twimg.com/media/CS-Mn7iW4AA3Tak.png'
-        expect(CollectTwitterMedia.media_filename(raw_media_uri)).to eq('CS-Mn7iW4AA3Tak.png')
+          it 'max_tweet_idメソッドは、ツイートIDを要素とする配列から、最大のツイートIDの値を抽出する' do
+            expect(CollectTwitterMedia.max_tweet_id(@tweet_id_collection)).to eq('661902288961871875')
+          end
+        end
       end
     end
-  end
 
-  describe 'JSON from REST API is correct?' do
-    it 'Via_client is correct' do
-      allow(@tweet_mock).to receive(:source).and_return("<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>")
-      expect(CollectTwitterMedia.via_client(@tweet_mock)).to eq('Twitter Web Client')
-    end
+    describe 'tweetオブジェクト' do
+      context '#original_tweet' do
+        it 'tweetオブジェクトがリツイートでないならば返り値は元のtweetオブジェクトである' do
+          allow(@tweet_mock).to receive(:retweet?).and_return(false)
 
-    it 'Tweet_id_collection is correct' do
-      allow(@tweet_mock).to receive(:id).and_return('661902288961871873')
-      expect(CollectTwitterMedia.tweet_id_collection(@tweets_mock).class).to eq(Array)
-      expect(CollectTwitterMedia.tweet_id_collection(@tweets_mock).count).to eq(@tweets_mock.count)
-    end
-  end
+          twitter_client_mock = double('Twitter API Client Mock')
+          allow(CollectTwitterMedia).to receive(:twitter_client).and_return(twitter_client_mock)
 
-  describe 'file_operation' do
-    it 'CSV file is created' do
-      allow(CSV).to receive(:open).and_return(nil) # not create csv file actually
-
-      save_directory = 'foobar'
-      expect(CollectTwitterMedia.create_csv_file(save_directory).match(/#{save_directory}\/image_from_twitter_[0-9]{8}_[0-9]{6}\.csv/).class).to eq(MatchData)
-    end
-
-    it 'basename_of_image_file is picked up' do
-      image_filename = 'CS-Mn7iW4AA3Tak.png'
-      expect(CollectTwitterMedia.basename_of_image_file(image_filename)).to eq('CS-Mn7iW4AA3Tak')
-    end
-
-    # TODO: write about remove_image
-
-    context 'when to_pathname method is used' do
-      it 'the return value is full path connected with current directory if argv is filename' do
-        pwd = Pathname.new(Dir.pwd)
-        argv = 'CS-Mn7iW4AA3Tak.png'
-
-        expect(CollectTwitterMedia.to_pathname(argv)).to eq("#{pwd}/#{argv}")
+          @client = CollectTwitterMedia.twitter_client
+          expect(CollectTwitterMedia.original_tweet(@tweet_mock)).to eq(@tweet_mock)
+        end
       end
 
-      it 'the return value is full path connected with current directory if argv is relative directory/file name' do
-        pwd = Pathname.new(Dir.pwd)
-        argv = 'foo/bar'
+      context '#media_uris' do
+        it 'tweetオブジェクトがメディア（画像、動画）の情報を持っている場合にその情報を返すか' do
+          # dirty... should use 'Factory Bot' for instance...
+          media = [
+            @tweet_media_photo_mock,
+            @tweet_media_photo_mock,
+            @tweet_media_photo_mock,
+            @tweet_media_photo_mock,
+          ]
+          return_uri = 'https://pbs.twimg.com/media/CS-Mn7iW4AA3Tak.png'
+          expected_return = [
+            return_uri,
+            return_uri,
+            return_uri,
+            return_uri,
+          ]
 
-        expect(CollectTwitterMedia.to_pathname(argv)).to eq("#{pwd}/#{argv}")
+          allow(@tweet_mock).to receive('media?').and_return(true)
+          allow(@tweet_mock).to receive(:media).and_return(media)
+          allow(@tweet_media_photo_mock).to receive('instance_of?').with(Twitter::Media::Photo).and_return(true)
+          allow(@tweet_media_photo_mock).to receive(:media_url_https).and_return(return_uri)
+
+          expect((CollectTwitterMedia.media_uris(@tweet_mock))).to eq(expected_return) # Array
+        end
+      end
+    end
+
+    describe '取得したテキストの加工' do
+      context "メディア（画像、動画）" do
+        it "生の URI から 'original' のサイズのメディアを取得できる URI に変換する" do # TODO: 動画の場合が考慮されていない
+          raw_media_uri = 'https://pbs.twimg.com/media/CS-Mn7iW4AA3Tak.png'
+          expect(CollectTwitterMedia.media_original_uri(raw_media_uri)).to eq('https://pbs.twimg.com/media/CS-Mn7iW4AA3Tak.png:orig')
+        end
+
+        it "ファイル名の部分を抽出する" do
+          raw_media_uri = 'https://pbs.twimg.com/media/CS-Mn7iW4AA3Tak.png'
+          expect(CollectTwitterMedia.media_filename(raw_media_uri)).to eq('CS-Mn7iW4AA3Tak.png')
+        end
+
+        it 'メディアファイルのファイル名の部分を、拡張子を除いて抽出する' do
+          image_filename = 'CS-Mn7iW4AA3Tak.png'
+          expect(CollectTwitterMedia.basename_of_image_file(image_filename)).to eq('CS-Mn7iW4AA3Tak')
+        end
       end
 
-      it 'the return value is the very argv itself if argv is absolute directory name' do
-        pwd = Pathname.new(Dir.pwd)
-        argv = '/foo/bar'
+      it 'ツイートしたクライアント名を抽出する' do
+        allow(@tweet_mock).to receive(:source).and_return("<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>")
+        expect(CollectTwitterMedia.via_client(@tweet_mock)).to eq('Twitter Web Client')
+      end
+    end
 
-        expect(CollectTwitterMedia.to_pathname(argv)).to eq("#{argv}")
+    describe 'ファイル操作' do
+      context '#create_csv_file' do
+        it 'CSVファイルが生成される' do
+          allow(CSV).to receive(:open).and_return(nil) # not create csv file actually
+
+          save_directory = 'foobar'
+          expect(CollectTwitterMedia.create_csv_file(save_directory).match(/#{save_directory}\/image_from_twitter_[0-9]{8}_[0-9]{6}\.csv/).class).to eq(MatchData)
+        end
       end
 
-      it 'the return value is full path connected with current directory if argv is directory/file name' do
-        pwd = Pathname.new(Dir.pwd)
-        argv = 'foo'
+      context '#to_pathname' do
+        context '引数' do
+          it '単純なディレクトリ名やファイル名の指定ならば、フルパスを返す' do
+            pwd   = Pathname.new(Dir.pwd)
+            argv  = 'CS-Mn7iW4AA3Tak.png'
 
-        expect(CollectTwitterMedia.to_pathname(argv)).to eq("#{pwd}/#{argv}")
+            expect(CollectTwitterMedia.to_pathname(argv)).to eq("#{pwd}/#{argv}")
+          end
+
+          it '相対ディレクトリ名や相対ファイル名の指定ならば、フルパスを返す' do
+            pwd   = Pathname.new(Dir.pwd)
+            argv  = 'foo/bar'
+
+            expect(CollectTwitterMedia.to_pathname(argv)).to eq("#{pwd}/#{argv}")
+          end
+
+          it '絶対指定ならば、そのままの値を返す' do
+            pwd   = Pathname.new(Dir.pwd)
+            argv  = '/foo/bar'
+
+            expect(CollectTwitterMedia.to_pathname(argv)).to eq("#{argv}")
+          end
+        end
       end
     end
   end
